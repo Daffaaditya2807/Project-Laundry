f7f3a14d448b185f731133bf3deeee85666c69a7
Daffaaditya2807 <e41211358@student.polije.ac.id>
2022-11-07 22:08:17 +0700

    Selesai Sampai Login

--- app/src/main/java/com/example/titulaundry/Login.java
+++ app/src/main/java/com/example/titulaundry/Login.java
@@ -2,10 +2,13 @@ package com.example.titulaundry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +21,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        notif(Login.this);
 
         takonAkun = (TextView) findViewById(R.id.takonAkun);
         String text = "<font color=#333333>Belum Punya Akun?</font> <font color=#2f80ed> Sign Up</font>";
@@ -25,7 +29,16 @@ public class Login extends AppCompatActivity {
         signUp();
         LoginToDashboard();
     }
-
+    public void notif(Activity activity){
+        //change color notif bar
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.white));
+        //set icons notifbar
+        View decor = activity.getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
     public void signUp(){
         takonAkun = (TextView) findViewById(R.id.takonAkun);
         takonAkun.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/titulaundry/Register.java
+++ app/src/main/java/com/example/titulaundry/Register.java
@@ -2,8 +2,11 @@ package com.example.titulaundry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,9 +19,20 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        notif(Register.this);
 
         takonAkun = (TextView) findViewById(R.id.takonAkun);
         String text = "<font color=#333333>Sudah Punya Akun?</font> <font color=#2f80ed> Sign In</font>";
         takonAkun.setText(Html.fromHtml(text));
+    }
+    public void notif(Activity activity){
+        //change color notif bar
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.white));
+        //set icons notifbar
+        View decor = activity.getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 }
