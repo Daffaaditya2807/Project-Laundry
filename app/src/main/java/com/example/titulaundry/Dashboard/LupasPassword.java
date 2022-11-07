3d03576bb4f99f53b056306e756c11421039080e
fikrilal <fikrimuzakky24@gmail.com>
2022-11-08 05:06:00 +0700

    add reset password

---
+++ app/src/main/java/com/example/titulaundry/Dashboard/LupasPassword.java
@@ -0,0 +1,14 @@
+package com.example.titulaundry.Dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LupasPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lupas_password);
+    }
+}
