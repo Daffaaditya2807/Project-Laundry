aa114b4f3fcc5c7b0380ee47a0d12df5b4404322
Daffa Aditya Rejasa Ruswanto <e41211358@student.polije.ac.id>
2022-11-09 10:22:42 +0700

    09-11-2022

--- app/src/main/java/com/example/titulaundry/atur_pesanan/pesanan.java
+++ app/src/main/java/com/example/titulaundry/atur_pesanan/pesanan.java
@@ -1,22 +1,35 @@
 package com.example.titulaundry.atur_pesanan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
+import com.example.titulaundry.Dashboard.MainMenu;
+import com.example.titulaundry.Dashboard.home_fragment;
+import com.example.titulaundry.Dashboard.order_fragment;
 import com.example.titulaundry.R;
 
 public class pesanan extends AppCompatActivity {
+    ImageButton toDashboard;
+    CardView setAlamat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesanan);
         notif(pesanan.this);
+        toKembali();
+        setAlamat();
     }
     public void notif(Activity activity){
         //change color notif bar
@@ -27,5 +40,24 @@ public class pesanan extends AppCompatActivity {
         //set icons notifbar
         View decor = activity.getWindow().getDecorView();
         decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+    public void setAlamat(){
+        setAlamat = (CardView) findViewById(R.id.menu1);
+        setAlamat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),set_waktu_alamat.class);
+                startActivity(i);
+            }
+        });
+    }
+    public void toKembali(){
+        toDashboard = (ImageButton) findViewById(R.id.kembali);
+        toDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pesanan.super.onBackPressed();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/titulaundry/atur_pesanan/set_waktu_alamat.java
@@ -0,0 +1,31 @@
+package com.example.titulaundry.atur_pesanan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.titulaundry.R;
+
+public class set_waktu_alamat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_waktu_alamat);
+        notif(set_waktu_alamat.this);
+    }
+    public void notif(Activity activity){
+        //change color notif bar
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.white));
+        //set icons notifbar
+        View decor = activity.getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+}
