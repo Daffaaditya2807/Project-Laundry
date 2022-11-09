24689830b939af99a038641fc1913a1b41a76920
fikrilal <fikrimuzakky24@gmail.com>
2022-11-09 23:32:06 +0700

    completed reset password

---
+++ app/src/main/java/com/example/titulaundry/lupaPassword3.java
@@ -0,0 +1,14 @@
+package com.example.titulaundry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lupaPassword3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lupa_password3);
+    }
+}
---
+++ app/src/main/java/com/example/titulaundry/lupaPasswordSucces.java
@@ -0,0 +1,14 @@
+package com.example.titulaundry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lupaPasswordSucces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lupa_password_succes);
+    }
+}
