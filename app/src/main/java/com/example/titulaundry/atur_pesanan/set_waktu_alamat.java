16146c4b1a3db8fa662e99d33efb6f89bacd7c1b
Daffa Aditya Rejasa Ruswanto <e41211358@student.polije.ac.id>
2022-11-13 06:18:21 +0700

    menambahkan picker tanggal

--- app/src/main/java/com/example/titulaundry/atur_pesanan/set_waktu_alamat.java
+++ app/src/main/java/com/example/titulaundry/atur_pesanan/set_waktu_alamat.java
@@ -4,25 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.example.titulaundry.R;
 
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class set_waktu_alamat extends AppCompatActivity {
     RadioButton rBtn1 , rBtn2;
+    DatePickerDialog picker;
     ConstraintLayout viewMenu;
+    TextView tgl1 , tgl2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_waktu_alamat);
         notif(set_waktu_alamat.this);
+        PilihTanggal();
         checkedButton();
     }
     public void notif(Activity activity){
@@ -34,6 +45,48 @@ public class set_waktu_alamat extends AppCompatActivity {
         //set icons notifbar
         View decor = activity.getWindow().getDecorView();
         decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+
+    public String getMonth(int month) {
+        return new DateFormatSymbols().getMonths()[month-1];
+    }
+
+    public void PilihTanggal(){
+        tgl1 = (TextView) findViewById(R.id.TanggalJemput1);
+        tgl2 = (TextView) findViewById(R.id.TanggalJemput2);
+
+        final Calendar cldr = Calendar.getInstance();
+        int day = cldr.get(Calendar.DAY_OF_MONTH);
+        int month = cldr.get(Calendar.MONTH);
+        int year = cldr.get(Calendar.YEAR);
+        tgl1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                picker = new DatePickerDialog(set_waktu_alamat.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month ,int day) {
+
+
+                        tgl1.setText(String.valueOf(day) + " " + getMonth((month+1)) + " " + String.valueOf(year));
+                    }
+                },year,month,day);
+                picker.show();
+            }
+        });
+        tgl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                picker = new DatePickerDialog(set_waktu_alamat.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month ,int day) {
+
+
+                        tgl2.setText(String.valueOf(day) + " " + getMonth((month+1)) + " " + String.valueOf(year));
+                    }
+                },year,month,day);
+                picker.show();
+            }
+        });
     }
 
     public void checkedButton(){
